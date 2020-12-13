d68299ef7fa4baa0ac9a4e0f9d0d9edb81b2bd15
wdlkmpx <wdlkmpx@gmail.com>
2020-12-13 13:35:19 +0800

    highscores.c: (dlg) replace GtkTable with GtkTreeView

--- src/highscore.c
+++ src/highscore.c
@@ -40,13 +40,9 @@ void write_highscore()
 
 void show_highscore(int place)
 {
-	GtkWidget *highscore_border;
 	GtkWidget * dialog;
-	GtkWidget *label;
-	GtkWidget *table;
 	GtkWidget *vbox, * button;
-	int temp;
-	char dummy[40];
+	int i, temp;
 
 	dialog = gtk_dialog_new ();
 	gtk_window_set_title (GTK_WINDOW (dialog), "Highscores");
@@ -56,71 +52,64 @@ void show_highscore(int place)
 	gtk_window_set_resizable (GTK_WINDOW (dialog), FALSE);
 	gtk_window_set_skip_pager_hint (GTK_WINDOW (dialog), TRUE);
 	gtk_window_set_skip_taskbar_hint (GTK_WINDOW (dialog), TRUE);
-	gtk_container_set_border_width (GTK_CONTAINER (dialog), 3);
+	//gtk_container_set_border_width (GTK_CONTAINER (dialog), 3);
 
 	vbox = gtk_dialog_get_content_area (GTK_DIALOG (dialog));
 
-	highscore_border = gtk_frame_new (NULL);
-	gtk_widget_show (highscore_border);
-	gtk_box_pack_start (GTK_BOX (vbox), highscore_border, TRUE, TRUE, 5);
+	//-----GtkTreeView
+	GtkTreeModel     * tree_model;
+	GtkTreeView      * treeview;
+	GtkTreeSelection * tree_sel;
+	GtkTreeIter      iter;
+	GtkListStore     * store;
 
-	table = gtk_table_new(NUM_HIGHSCORE+1,5,FALSE);
-	gtk_container_add (GTK_CONTAINER (highscore_border),table);
+	store = gtk_list_store_new (5,
+	                            G_TYPE_INT,    /*   #   */
+	                            G_TYPE_STRING, /* name  */
+	                            G_TYPE_INT,    /* lines */
+	                            G_TYPE_INT,    /* level */
+	                            G_TYPE_INT);   /* score */
+	tree_model = GTK_TREE_MODEL (store);
 
-	label = gtk_label_new(" # ");
-	gtk_widget_show(label);
-	gtk_table_attach_defaults(GTK_TABLE(table),label,0,1,0,1);
-	gtk_widget_set_size_request(label,25,0);
-	
-	label = gtk_label_new(" Name: ");
-	gtk_widget_show(label);
-	gtk_table_attach_defaults(GTK_TABLE(table),label,1,2,0,1);
-	gtk_widget_set_size_request(label,100,0);
-	
-	label = gtk_label_new(" Lines: ");
-	gtk_widget_show(label);
-	gtk_table_attach_defaults(GTK_TABLE(table),label,2,3,0,1);
-	
-	label = gtk_label_new(" Level: ");
-	gtk_widget_show(label);
-	gtk_table_attach_defaults(GTK_TABLE(table),label,3,4,0,1);
-	
-	label = gtk_label_new(" Score: ");
-	gtk_widget_show(label);
-	gtk_table_attach_defaults(GTK_TABLE(table),label,4,5,0,1);
-	
-	for(temp=0;temp < NUM_HIGHSCORE;temp++)
+	treeview = GTK_TREE_VIEW (gtk_tree_view_new_with_model (tree_model));
+
+	tree_sel = gtk_tree_view_get_selection (treeview);
+	gtk_tree_selection_set_mode (tree_sel, GTK_SELECTION_NONE); //_SINGLE _MULTIPLE
+
+	g_object_unref (tree_model);
+
+	/* COLUMNS */
+	static const char * titles[] = { "#", "Name", "Lines", "Level", "Score" };
+	GtkCellRenderer * r[5];
+	GtkTreeViewColumn * col[5];
+	for (i = 0; i < 5; i++)
 	{
-		sprintf(dummy,"%d",temp+1);
-		label = gtk_label_new(dummy);
-		gtk_table_attach_defaults(GTK_TABLE(table),label,0,1,temp+1,temp+2);
-		gtk_misc_set_alignment(GTK_MISC(label),0.5,0);	
-		
-		sprintf(dummy,"%s",highscore[temp].name);
-      		label = gtk_label_new(dummy);
-
-		if(place && place-1 == temp)
-		  set_label_with_color (label, "blue", dummy);
-		gtk_table_attach_defaults(GTK_TABLE(table),label,1,2,temp+1,temp+2);
-		gtk_misc_set_alignment(GTK_MISC(label),0.5,0);
-
-		sprintf(dummy,"%d",highscore[temp].lines);
-		label = gtk_label_new(dummy);
-		gtk_table_attach_defaults(GTK_TABLE(table),label,2,3,temp+1,temp+2);
-		gtk_misc_set_alignment(GTK_MISC(label),0.5,0);
-
-		sprintf(dummy,"%d",highscore[temp].level);
-		label = gtk_label_new(dummy);
-		gtk_table_attach_defaults(GTK_TABLE(table),label,3,4,temp+1,temp+2);
-		gtk_misc_set_alignment(GTK_MISC(label),0.5,0);
-
-		sprintf(dummy,"%lu",highscore[temp].score);
-		label = gtk_label_new(dummy);
-		if(place && place-1 == temp)
-		  set_label_with_color (label, "red", dummy);
-		gtk_table_attach_defaults(GTK_TABLE(table),label,4,5,temp+1,temp+2);
-		gtk_misc_set_alignment(GTK_MISC(label),1,0);
+		r[i] = g_object_new (GTK_TYPE_CELL_RENDERER_TEXT, "xalign", 0.5, NULL);
+		col[i] = g_object_new (GTK_TYPE_TREE_VIEW_COLUMN,
+		                       "title",          titles[i],
+		                       "sort-column-id", i,
+		                       "alignment",      0.5,
+		                       "sizing",         GTK_TREE_VIEW_COLUMN_AUTOSIZE,
+		                       NULL);
+		gtk_tree_view_column_pack_start (col[i], r[i], TRUE);
+		gtk_tree_view_column_add_attribute (col[i], r[i], "text", i);
+		gtk_tree_view_append_column (treeview, col[i]);
 	}
+
+	for(temp = 0; temp < NUM_HIGHSCORE; temp++)
+	{
+		gtk_list_store_append (store, &iter);
+		gtk_list_store_set (store, &iter,
+		                    0, temp+1,
+		                    1, highscore[temp].name,
+		                    2, highscore[temp].lines,
+		                    3, highscore[temp].level,
+		                    4, highscore[temp].score,
+		                    -1);
+	}
+
+	gtk_box_pack_start (GTK_BOX (vbox), GTK_WIDGET (treeview), TRUE, TRUE, 3);
+	//-----End of GtkTreeView
 
 	button = gtk_dialog_add_button (GTK_DIALOG (dialog), "gtk-close", GTK_RESPONSE_CLOSE);
 	gtk_widget_grab_focus (button);
